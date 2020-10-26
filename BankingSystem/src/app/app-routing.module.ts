8ee8608252737eb7323c0d6595568eaeb294200b
Nighthawk <jb@Nighthawks-MBP.attlocal.net>
2020-10-26 17:36:41 -0500

    Basic Application display

--- BankingSystem/src/app/app-routing.module.ts
+++ BankingSystem/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoanApplicationComponent } from './components/loan-application/loan-application.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'loan', component: LoanApplicationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BankingSystem/src/app/app.module.ts
+++ BankingSystem/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
-import { LoanApplicationComponent } from './loan-application/loan-application.component';
+import { LoanApplicationComponent } from './components/loan-application/loan-application.component';
 
 @NgModule({
   declarations: [
